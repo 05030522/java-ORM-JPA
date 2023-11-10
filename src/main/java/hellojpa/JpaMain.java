f943a1a4db42eff1a30a8a53ab4f6a9e8c28de2a
05030522 <kjinhx96@gmail.com>
2023-11-11 08:31:07 +0900

    jpa h2 동작확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,12 +22,21 @@ public class JpaMain {
         emf.close();*/
 
         try {
-            Member findMember = em.find(Member.class, 2L);
+//            Member findMember = em.find(Member.class, 2L);
 //            System.out.println("findMember.id = " + findMember.getId());
 //            System.out.println("findMember.name = " + findMember.getName());
-
 //            em.remove(findMember);
-            findMember.setName("HelloJPA");
+//            findMember.setName("HelloJPA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+            //jpa는 테이블을 대상으로 코드를 절대짜지않음, 멤버 객체를 대상으로 코드를 작성함
+
+            for (Member member : result){
+                System.out.println("member = " + member.getName());
+            }
 
             tx.commit();
         }catch (Exception e){
