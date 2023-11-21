bbb98139c46dfda853eb3ed1f92b4ed927c645cc
05030522 <kjinhx96@gmail.com>
2023-11-21 09:30:08 +0900

    객체와 테이블 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,21 +22,36 @@ public class JpaMain {
         emf.close();*/
 
         try {
-//            Member findMember = em.find(Member.class, 2L);
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.find(Member.class, 200L);
+            member.setName("aaaa");
+
+            em.detach(member);
+
+            em.flush();
+            System.out.println("========================");
+
+
+
+//            em.clear(); //영속성 컨텍스트를 완전 초기화
+
+            em.find(Member.class, 2L);
 //            System.out.println("findMember.id = " + findMember.getId());
 //            System.out.println("findMember.name = " + findMember.getName());
 //            em.remove(findMember);
 //            findMember.setName("HelloJPA");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            //jpa는 테이블을 대상으로 코드를 절대짜지않음, 멤버 객체를 대상으로 코드를 작성함
-
-            for (Member member : result){
-                System.out.println("member = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            //jpa는 테이블을 대상으로 코드를 절대짜지않음, 멤버 객체를 대상으로 코드를 작성함
+//
+//            for (Member member : result){
+//                System.out.println("member = " + member.getName());
+//            }
 
             tx.commit();
         }catch (Exception e){
