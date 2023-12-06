d7d1dec4ff324a32e15f4054a20786ed8655283f
05030522 <kjinhx96@gmail.com>
2023-12-06 23:07:21 +0900

    실전예제_요구사항분석_기본매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -8,12 +8,9 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-
         try {
             tx.commit();
         }catch (Exception e){
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,9 +9,14 @@ public class Order {
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    @Column(name = "MEMBER_ID")
+    @Column(name = "MEMBER_ID") //외래키를 객체에 그대로 가져와서 객체지향적이지 않음
     private Long memberId;
-    private LocalDateTime orderDate;
+//    private Member member;
+//    public Member getMember() {
+//        return member;
+//    }
+
+    private LocalDateTime orderDate; //ORDER_DATE , order_date
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
